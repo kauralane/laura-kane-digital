4ad9e23245dbf86a9fbd3cdef843092871afbe43
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-21 22:26:23 +0000

    added modal form to contact page.

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import './contact.css'
+
 const Contact = () => {
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
 return (
     <>
@@ -12,8 +23,50 @@ return (
         <a style={{ color: 'black' }} href='https://github.com/kauralane'>
     <h2>GITHUB</h2>
         </a>
-    <h2>CONTACT FORM</h2>
-    {/* Make this so that when you click on the link a modal comes up with a form? */}
+        <a onClick={handleShow}>
+    <h2 id="contactFormLink">CONTACT FORM</h2>
+        </a>
+
+        <Modal show={show} onHide={handleClose} style={{ fontFamily: "Archivo Black, sans-serif" }}>
+            <Modal.Header closeButton>
+                <Modal.Title>Get In Touch</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="name"
+                            placeholder="Name"
+                            autoFocus
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="name@example.com"
+                            autoFocus
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        className="mb-3"
+                        controlId="enquiry"
+                    >
+                        <Form.Label>Enquiry</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="light" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="dark" onClick={handleClose}>
+                    Submit
+                </Button>
+            </Modal.Footer>
+        </Modal>
     </>
 )
 }

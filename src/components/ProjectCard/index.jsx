18810222d52b6b4bb384cef77ccea667ce7c5757
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-21 19:09:56 +0000

    added images for all pportfolio cards and styled cards.

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -3,15 +3,16 @@ import Card from 'react-bootstrap/Card';
 
 function ProjectCard({ project }) {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={project.image} />
-            <Card.Body>
-                <Card.Title>{project.title}</Card.Title>
+        <Card border="dark" style={{ width: '50rem', textAlign: 'center', marginLeft: 50, marginBottom: 10}}>
+            <Card.Img variant="bottom" src={project.image} className="projectScreenshot" style= {{ maxHeight: 300, objectFit: 'cover', objectPosition: 'top left' }} />
+            <Card.Body style={{ fontFamily: "Archivo Black, sans-serif", fontStyle: 'normal', textAlign: 'left', backgroundColor: 'var(--blue-color)'}}>
+                <Card.Title className="title">{project.title}</Card.Title>
                 <Card.Text>
                     {project.description}
                 </Card.Text>
-                <a href={project.deployed}><Button variant="primary">Deployed Application</Button></a>
-                <a href={project.github}><Button variant="primary">GitHub Repository</Button></a>
+                {/* Make these links open on new tab! */}
+                <a href={project.deployed} style={{ marginRight: 10 }}><Button variant="dark">Deployed Application</Button></a>
+                <a href={project.github}><Button variant="dark">GitHub Repository</Button></a>
             </Card.Body>
         </Card>
     );

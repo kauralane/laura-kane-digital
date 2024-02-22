288a886ab8608a128627936f9d7c6b175ffc8c2e
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-22 20:58:57 +0000

    changed single quotations to double quotations on routes.

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
       <MyNavbar />
 
       <Routes>
-        <Route path='/work' element={< Work />} />
-        <Route path='/services' element={< Services />} />
-        <Route path='/contact' element={< Contact />} />
-        <Route path='/404' element={< Custom404 />} />
-        <Route path='' element={< Home />} />
+        <Route path="/work" element={< Work />} />
+        <Route path="/services" element={< Services />} />
+        <Route path="/contact" element={< Contact />} />
+        <Route path="/404" element={< Custom404 />} />
+        <Route path="" element={< Home />} />
       </Routes>
 
     </Router>

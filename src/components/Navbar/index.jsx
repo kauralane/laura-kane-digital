9268a656ce21972174e7a9b34c1dbf50fdccf8ea
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-23 16:23:08 +0000

    moved portfolio images to public folder.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './index.css'
 
 function MyNavbar() {
@@ -24,4 +23,4 @@ function MyNavbar() {
     );
 }
 
-export default MyNavbar
+export default MyNavbar;

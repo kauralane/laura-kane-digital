0fb7b824b349a7f57e3b8047142032ca6fb8c9a2
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-23 15:21:58 +0000

    added ./ to beginning of navbar links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,13 +9,13 @@ function MyNavbar() {
         <Navbar expand="lg" className="customNav"> 
         {/* change navbar colours and styling here */}
             <Container>
-                <Navbar.Brand id="navTitle" href="/">LAURA KANE DIGITAL</Navbar.Brand>
+                <Navbar.Brand id="navTitle" href="./">LAURA KANE DIGITAL</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link className="navLink" href="/">home</Nav.Link>
-                        <Nav.Link className="navLink" href="/work">work</Nav.Link>
-                        <Nav.Link className="navLink" href="/services">services</Nav.Link>
+                        <Nav.Link className="navLink" href="./">home</Nav.Link>
+                        <Nav.Link className="navLink" href="./work">work</Nav.Link>
+                        <Nav.Link className="navLink" href="./services">services</Nav.Link>
                         {/* <NavDropdown title="services" id="basic-nav-dropdown">
                             <NavDropdown.Item href="webDev">front-end web development</NavDropdown.Item>
                             <NavDropdown.Item href="#socialMedia">social media</NavDropdown.Item>
